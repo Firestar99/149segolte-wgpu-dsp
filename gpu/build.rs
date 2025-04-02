57ed4ac64c5381c384e2406da69f41f7672119cc
149segolte <37300847+149segolte@users.noreply.github.com>
2025-04-02 12:53:47 -0400

    refactor: move to single kernel

--- gpu/build.rs
+++ gpu/build.rs
@@ -1,12 +1,12 @@
+use std::path::Path;
+
 use spirv_builder::{MetadataPrintout, SpirvBuilder};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    for kernel in std::fs::read_dir("kernels")? {
-        let path = kernel?.path();
-        SpirvBuilder::new(path, "spirv-unknown-vulkan1.1")
-            .capability(spirv_builder::Capability::Int16)
-            .print_metadata(MetadataPrintout::Full)
-            .build()?;
-    }
+    let path = Path::new("compute_shader");
+    SpirvBuilder::new(path, "spirv-unknown-vulkan1.1")
+        .capability(spirv_builder::Capability::Int16)
+        .print_metadata(MetadataPrintout::Full)
+        .build()?;
     Ok(())
 }
